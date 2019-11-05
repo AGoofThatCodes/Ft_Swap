f5ba8c87430b9c1c556ed6f823c2ffae22b881b9
Kirsty Selesho (Britehouse) <Kirsty.Selesho@britehouse.co.za>
2019-11-05 12:24:54 +0200

    back to the bootcamp days

---
+++ Ft_Swap1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ft_Swap1
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a {0} ", a);
+            Console.WriteLine("b {0} ", b);
+            Console.ReadLine();
+
+            
+
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
